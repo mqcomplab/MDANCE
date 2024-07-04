d99e7d32676065313f227d8b9a78fe8471958c8f
lexin-chen <lxchen017@gmail.com>
2024-07-04 14:05:46 -0400

    change arg to dict for argparse

--- src/mdance/prime/rep_frames_cl.py
+++ src/mdance/prime/rep_frames_cl.py
@@ -13,16 +13,20 @@ No trim, RR index
 from mdance.prime.rep_frames import gen_one_method_max, gen_all_methods_max
 import argparse
 
+parser_dict = {
+    'method': {'type': str, 'help': 'method to use'},
+    'sim_folder': {'type': str, 'help': 'folder to access'},
+    'trim_frac': {'type': float, 'default': None, 'help': 'Trim parameter for gen_method_max method'},
+    'index': {'type': str, 'default': 'RR', 'help': 'n_ary parameter for gen_method_max method'},
+    'norm_folder': {'type': str, 'help': 'norm_folder to access'}
+}
+
 parser = argparse.ArgumentParser(description='Generate method max with optional trim and n_ary')
-parser.add_argument('-m', '--method', type=str, help='method to use')
-parser.add_argument('-s', '--sim_folder', type=str, help='folder to access')
-parser.add_argument('-t', '--trim_frac', type=float, default=None,
-                    help='Trim parameter for gen_method_max method')
-parser.add_argument('-i', '--index', type=str, default='RR',
-                    help='n_ary parameter for gen_method_max method')
-parser.add_argument('-d', '--norm_folder', type=str, help='norm_folder to access')
+for key, value in parser_dict.items():
+    parser.add_argument(f'-{key[0]}', f'--{key}', **value) 
 
 args = parser.parse_args()
+
 if args.method:
     gen_one_method_max(method=args.method, sim_folder=args.sim_folder, norm_folder=args.norm_folder, 
                        trim_frac=args.trim_frac, n_ary=args.index)
