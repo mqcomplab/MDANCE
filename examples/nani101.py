3fa35e64e805aa34766bbde7471ec37155f902b9
lexin-chen <lxchen017@gmail.com>
2024-07-07 14:45:09 -0400

    update nani101.py example

--- examples/nani101.py
+++ examples/nani101.py
@@ -10,30 +10,53 @@ deterministic, converges faster, and finds better solutions.
 """
 
 ###############################################################################
-# Imports
-#   - ``numpy <https://numpy.org/>`` for manipulating arrays.
-#   - ``sci-kit-learn <https://scikit-learn.org/stable/>`` for *k*-means clustering.
-#   - ``mdance.cluster.nani <https://mdance.readthedocs.io/en/latest/api/mdance.cluster.nani.html>``
+# Let's start with importing necessary libraries.
 
 import numpy as np
 from sklearn.cluster import KMeans
+from sklearn.datasets import make_blobs
+from matplotlib import pyplot as plt
 from mdance.cluster.nani import KmeansNANI
+
 ###############################################################################
 # Data
 #   - Load the data from a file, must be array of shape (n_samples, n_features).
-#   - In this example, we use a numpy file.
+#   - In this example, we will generate synthetic data using ``make_blobs`` from Fig. 2 of the `NANI paper <https://pubs.acs.org/doi/10.1021/acs.jctc.4c00308>`_.
 
-data = np.load('data.npy')
+n_clusters = 7
+data, true_labels = make_blobs(n_samples=1000, centers=n_clusters, n_features=2, random_state=0)
+
 ###############################################################################
+# First, let's checkout how state-of-the-art *k*-means performs on the data.
+# which uses *k*-means++ initialization.
+
+og_kmeans = KMeans(n_clusters=n_clusters, init='k-means++', n_init=1, random_state=42)
+og_kmeans.fit(data)
+og_kmeans_labels = og_kmeans.labels_
+
+###############################################################################
+# Visualize the clustered results to true labels.
+
+fig1, ax1 = plt.subplots(1, 2, figsize=(12, 8), sharex=True, sharey=True)
+ax1[0].scatter(data[:, 0], data[:, 1], c=og_kmeans_labels, cmap='tab10', s=20)
+ax1[0].set_title('K-means++ Labels', fontsize=16, fontweight='bold')
+ax1[1].scatter(data[:, 0], data[:, 1], c=true_labels, cmap='tab10', s=20)
+ax1[1].set_title('True Labels', fontsize=16, fontweight='bold')
+plt.show()
+
+###############################################################################
+# As you can see, *k*-means++ initialization did not get it right. 
+# Let's use NANI to optimize initial centroids.
 # NANI
 #   - Create an instance of KmeansNANI.
 #   - ``data``: data to cluster.
 #   - ``n_clusters``: number of clusters.
 
-
-mod = KmeansNANI(data=data, n_clusters=4, metric='MSD', N_atoms=1, 
+mod = KmeansNANI(data=data, n_clusters=n_clusters, metric='MSD', N_atoms=1, 
                  init_type='comp_sim', percentage=10)
 initiators = mod.initiate_kmeans()
+initiators = initiators[:n_clusters]
+
 ###############################################################################
 # K-means
 #   - Create an instance of KMeans.
@@ -42,13 +65,25 @@ initiators = mod.initiate_kmeans()
 #  - ``n_init``: NANI only needs one initialization!
 #  - ``random_state``: We don't need this because NANI is 100% deterministic!
 
-kmeans = KMeans(n_clusters=4, init=initiators, n_init=1, random_state=None)
+kmeans = KMeans(n_clusters=n_clusters, init=initiators, n_init=1, random_state=None)
 kmeans.fit(data)
 kmeans_labels = kmeans.labels_
+###############################################################################
+# Plot
+#   - Plot the data with different colors for each cluster.
+
+fig, ax2 = plt.subplots(1, 2, figsize=(12, 8), sharex=True, sharey=True)
+ax2[0].scatter(data[:, 0], data[:, 1], c=kmeans_labels, cmap='tab10', s=20)
+ax2[0].set_title('K-means Labels', fontsize=16, fontweight='bold')
+ax2[1].scatter(data[:, 0], data[:, 1], c=true_labels, cmap='tab10', s=20)
+ax2[1].set_title('True Labels', fontsize=16, fontweight='bold')
+plt.show()
 
 ###############################################################################
+# As you can see, NANI clustered the data perfectly!
+#
 # That's it! You have successfully used NANI to optimize initial centroids for *k*-means clustering.
 #   - ``kmeans_labels``: cluster labels assigned by *k*-means using NANI.
-
-# For more advance usage, please look at the NANI Tutorial<https://mdance.readthedocs.io/en/latest/nani.html>!
+#
+# For more advance usage, please look at the `NANI Tutorial <../tutorials/nani.html>`_.
 # Why? Because NANI can also predict number of clusters, work with Molecular Dynamics data, and more!
