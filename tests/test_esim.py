c7415fd79d303c66d4e49a4d5ea46e065d7bc886
lexin-chen <lxchen017@gmail.com>
2024-07-30 21:08:37 -0400

    Update tests
    Update tests according to new `mdance.data` functionality. Now,
    directory path is not needed. pytest should work from any path that
    accesses the `tests` folder.

---
+++ tests/test_esim.py
@@ -0,0 +1,75 @@
+import numpy as np
+import pytest
+
+import mdance.tools.esim as esim
+
+@pytest.fixture(scope='module')
+def bit_data():
+    arr = np.array(
+        [[0, 1, 0, 0, 1, 0],
+         [1, 0, 1, 1, 0, 1],
+         [1, 0, 0, 0, 1, 1],
+         [1, 1, 0, 1, 1, 1],
+         [0, 1, 1, 0, 1, 1]
+        ])
+    return arr
+
+
+@pytest.fixture(scope='module')
+def c_sum(bit_data):
+    return np.sum(bit_data, axis=0)
+
+
+@pytest.fixture(scope='module')
+def n_objects(bit_data):
+    return len(bit_data)
+
+
+def test_calculate_counters(c_sum, n_objects):
+    counter_dict = esim.calculate_counters(c_sum, n_objects)
+    expected_dict = {
+        'a': 2, 'w_a': 1.2, 
+        'd': 0, 'w_d': 0.0, 
+        'total_sim': 2, 'total_w_sim': 1.2, 
+        'total_dis': 4, 'total_w_dis': 4.0, 
+        'p': 6, 'w_p': 5.2}
+    assert counter_dict == expected_dict
+
+
+def test_gen_sim_dict(c_sum, n_objects):
+    sim_dict = esim.gen_sim_dict(c_sum, n_objects)
+    expected_dict = {
+        'BUB': 0.2, 'Fai': 0.2, 
+        'Gle': 0.3, 'Ja': 0.36, 
+        'JT': 0.2, 'RT': 0.12, 
+        'RR': 0.2, 'SM': 0.2, 
+        'SS1': 0.12, 'SS2': 0.3
+        }
+    # Give a tolerance of 1e-10
+    for key in sim_dict:
+        assert abs(sim_dict[key] - expected_dict[key]) < 1e-10
+
+
+def test_calc_medoid(bit_data):
+    medoid_idx = esim.calc_medoid(bit_data)
+    assert medoid_idx == 3
+
+
+def test_calc_outlier(bit_data):
+    outlier_idx = esim.calc_outlier(bit_data)
+    assert outlier_idx == 0
+
+
+def test_calc_comp_sim(bit_data):
+    comp_sim = esim.calc_comp_sim(bit_data)
+    expected_comp_sim = [
+        (0, 1/3), 
+        (1, 1/3), 
+        (2, 1/4), 
+        (3, 1/6), 
+        (4, 1/4)
+        ]
+    # Give a tolerance of 1e-10
+    for i, j in zip(comp_sim, expected_comp_sim):
+        assert i[0] == j[0]
+        assert abs(i[1] - j[1]) < 1e-10
---
+++ tests/test_preprocess.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pytest
+
+import mdance.inputs.preprocess as pre
+from mdance import data
+
+
+@pytest.fixture(scope='module')
+def expected_traj_numpy():
+    traj_numpy = np.load(data.sim_traj_numpy)
+    return traj_numpy
+
+
+def test_gen_traj_numpy(expected_traj_numpy):
+    atom_sel = 'resid 3 to 12 and name N CA C O H'
+    traj_numpy = pre.gen_traj_numpy(data.top, data.traj, atom_sel)
+    assert np.allclose(traj_numpy, expected_traj_numpy)
+
+
+def test_Normalizer(expected_traj_numpy):
+    norm = pre.Normalizer(data=expected_traj_numpy)
+    matrix = norm.get_v3_norm()
+    min, max, avg = norm.get_min_max()
+    assert np.all(matrix >= 0) and np.all(matrix <= 1)
+    assert min == pytest.approx(17.97506)
+    assert max == pytest.approx(42.1838)
