676c325d864cab9f7f9bdb9cfba94f5161316627
lexin-chen <lxchen017@gmail.com>
2024-07-31 22:06:46 -0400

    Added docstrings & verbosity deletion

--- tests/test_esim.py
+++ tests/test_esim.py
@@ -5,6 +5,9 @@ import mdance.tools.esim as esim
 
 @pytest.fixture(scope='module')
 def bit_data():
+    """
+    A 5x6 binary matrix.
+    """
     arr = np.array(
         [[0, 1, 0, 0, 1, 0],
          [1, 0, 1, 1, 0, 1],
@@ -17,15 +20,24 @@ def bit_data():
 
 @pytest.fixture(scope='module')
 def c_sum(bit_data):
+    """
+    Column-wise sum of the matrix.
+    """
     return np.sum(bit_data, axis=0)
 
 
 @pytest.fixture(scope='module')
 def n_objects(bit_data):
+    """
+    Number of objects in the matrix.
+    """
     return len(bit_data)
 
 
 def test_calculate_counters(c_sum, n_objects):
+    """
+    Test the calculation of counters.
+    """
     counter_dict = esim.calculate_counters(c_sum, n_objects)
     expected_dict = {
         'a': 2, 'w_a': 1.2, 
@@ -37,6 +49,9 @@ def test_calculate_counters(c_sum, n_objects):
 
 
 def test_gen_sim_dict(c_sum, n_objects):
+    """
+    Test the generation of similarity dictionary.
+    """
     sim_dict = esim.gen_sim_dict(c_sum, n_objects)
     expected_dict = {
         'BUB': 0.2, 'Fai': 0.2, 
@@ -51,16 +66,25 @@ def test_gen_sim_dict(c_sum, n_objects):
 
 
 def test_calc_medoid(bit_data):
+    """
+    Test the calculation of medoid.
+    """
     medoid_idx = esim.calc_medoid(bit_data)
     assert medoid_idx == 3
 
 
 def test_calc_outlier(bit_data):
+    """
+    Test the calculation of outlier.
+    """
     outlier_idx = esim.calc_outlier(bit_data)
     assert outlier_idx == 0
 
 
 def test_calc_comp_sim(bit_data):
+    """
+    Test the calculation of complementary similarity.
+    """
     comp_sim = esim.calc_comp_sim(bit_data)
     expected_comp_sim = [
         (0, 1/3), 
--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -7,17 +7,25 @@ from mdance import data
 
 @pytest.fixture(scope='module')
 def expected_traj_numpy():
-    traj_numpy = np.load(data.sim_traj_numpy)
-    return traj_numpy
+    """
+    Load the expected trajectory data.
+    """
+    return np.load(data.sim_traj_numpy)
 
 
 def test_gen_traj_numpy(expected_traj_numpy):
+    """
+    Test the generation of a trajectory in numpy format.
+    """
     atom_sel = 'resid 3 to 12 and name N CA C O H'
     traj_numpy = pre.gen_traj_numpy(data.top, data.traj, atom_sel)
     assert np.allclose(traj_numpy, expected_traj_numpy)
 
 
 def test_Normalizer(expected_traj_numpy):
+    """
+    Test the Normalizer class.
+    """
     norm = pre.Normalizer(data=expected_traj_numpy)
     matrix = norm.get_v3_norm()
     min, max, avg = norm.get_min_max()
