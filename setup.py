acf6492822cea758ea86ed6ad7ce1d7de1de8a48
lexin-chen <lxchen017@gmail.com>
2024-08-02 10:45:00 -0400

    Revised correct dir path for `setup.py`.

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import versioneer
+
 
 setup(
     name='mdance',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    packages=['mdance'],  # Adjust this to match your package structure
-    package_dir={'': 'src'},  # Adjust this if your source code is in a different directory
+    package_dir={'': 'src'},
+    packages=find_packages("src")
 )
